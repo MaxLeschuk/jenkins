1d5f1dcb58574c16d4fedb4ca7a8b44708d2dd4b
MaxLeschuk <55053909+MaxLeschuk@users.noreply.github.com>
2025-04-15 13:40:16 +0200

    Update SpringApp.java

--- src/main/java/com/example/app/SpringApp.java
+++ src/main/java/com/example/app/SpringApp.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringApp {
-    private SpringApp() {
-        throw new UnsupportedOperationException("Utility class");
-    }
+
     /**
      * Main method to start the Spring Boot application.
-     * @param args Command-line arguments.
+
      */
     public static void main(final String[] args) {
         SpringApplication.run(SpringApp.class, args);
